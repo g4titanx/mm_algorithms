9db4c3a3628c46ecabc996dc2003cc07c06d33d7
g4titanx <g4titan1@gmail.com>
2024-07-17 15:49:05 +0100

    algoritm 5 - additive group

--- src/algorithm_5.rs
+++ src/algorithm_5.rs
@@ -1,6 +1,7 @@
 //algorithm 5 - Cyclic Group Exponentiation/ Square and Multiply
 //assuming G is a multiplicative group
-pub fn exponentiation(g: u64, x: u64, n: u64) -> u64 {
+#[allow(unused)] 
+pub fn exponentiation_m(g: u64, x: u64, n: u64) -> u64 {
     let mut h = g;
     let mut y = 1;
     let binary_x = format!("{:b}", x); 
@@ -17,12 +18,31 @@ pub fn exponentiation(g: u64, x: u64, n: u64) -> u64 {
     y
 }
 
+//for an additive group
+#[allow(unused)] 
+pub fn exponentiation_a(g: u64, x: u64, n: u64) -> u64 {
+    let mut h = g;
+    let mut y = 0;
+    let binary_x = format!("{:b}", x); 
+    let k = binary_x.len();
+
+    for j in 0..k {
+        let bj = binary_x.chars().nth(k - j - 1).unwrap();
+
+        if bj == '1' {
+            y = (y + h) % n; 
+        }
+        h = (h + h) % n;
+    }
+    y
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_exponentiation() {
+    fn test_exponentiation_m() {
         let g = 2; 
         let x = 13;
         let n = 17;
